c1f6fc05bdf0738ce6b75f5825a5dc49ddbf82e3
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2024-05-20 14:29:01 -0700

    Pushed from Domino: https://flows-demo37381.train-sandbox.domino.tech/workspace/integration-test/CDISC01_RE_FLOW?executionId=664bb7d64a0afb656bbe30f3

--- workflow_adam_tfl.py
+++ workflow_adam_tfl.py
@@ -4,14 +4,22 @@ from flytekit.types.file import FlyteFile,PDFFile
 from flytekit import WorkflowFailurePolicy
 from utils.adam import create_adam_data
 from utils.tfl import create_tfl_report
-from typing import TypeVar, NamedTuple
-from utils.adam import ADAM
-
-adam_outputs = NamedTuple("adam_outputs", adsl=FlyteFile[TypeVar("sas7bdat")], adae=FlyteFile[TypeVar("sas7bdat")], advs=FlyteFile[TypeVar("sas7bdat")], adcm=FlyteFile[TypeVar("sas7bdat")], adef=FlyteFile[TypeVar("sas7bdat")], adlb=FlyteFile[TypeVar("sas7bdat")], admh=FlyteFile[TypeVar("sas7bdat")])
-tfl_outputs = NamedTuple("tfl_outputs", t_ae_rel=PDFFile, t_vscat=PDFFile)
+from typing import TypeVar
 
 @workflow
-def create_adam_datasets(sdtm_data_path: str) -> adam_outputs:
+def workflow_full(sdtm_data_path: str) -> (PDFFile, PDFFile, PDFFile, PDFFile, PDFFile, PDFFile, PDFFile, PDFFile):
+    """
+    This script mocks a sample clinical trial using Domino Flows. 
+
+    The input to this flow is the path to your SDTM data. You can point this to either your SDTM-BLIND dataset or your SDTM-UNBLIND dataset. The output to this flow are a series of TFL reports.
+
+    To the run the workflow remotely, execute the following code in your terminal:
+    
+    pyflyte run --copy-all --remote workflow_full.py workflow_full --sdtm_data_path "/mnt/imported/data/snapshots/sdtm-blind/1"
+
+    :param sdtm_data_path: The root directory of your SDTM dataset
+    :return: A list of PDF files containing the TFL reports
+    """
     # Create task that generates ADSL dataset. This will run a unique Domino job and return its outputs.
     adsl = create_adam_data(
         name="ADSL", 
@@ -74,45 +82,85 @@ def create_adam_datasets(sdtm_data_path: str) -> adam_outputs:
         sdtm_data_path=sdtm_data_path, 
         dependencies=[adsl]
     )
-
-    return adam_outputs(adsl=adsl.data, adae=adae.data, advs=advs.data, adcm=adcm.data, adef=adef.data, adlb=adlb.data, admh=admh.data)
-
-@workflow
-def create_tfl_reports(adsl: FlyteFile[TypeVar("sas7bdat")], adae: FlyteFile[TypeVar("sas7bdat")], advs: FlyteFile[TypeVar("sas7bdat")]) -> tfl_outputs:
-    # Create task that generates TFL report from ADAE dataset.
+    # Create task that generates TFL report from T_POP table.
+    t_pop = create_tfl_report(
+       name="T_POP", 
+        command="prod/tfl/t_pop.sas", 
+        environment="SAS Analytics Pro",
+        hardware_tier= "Small",
+        dependencies=[adsl]
+    )
+    # Create task that generates TFL report from T_AE_REL table.
     t_ae_rel = create_tfl_report(
         name="T_AE_REL", 
-        command="sas -stdio prod/tfl/t_ae_rel.sas", 
+        command="prod/tfl/t_ae_rel.sas", 
         environment="SAS Analytics Pro",
         hardware_tier= "Small",
-        dependencies=[ADAM("adae", adae)]
+        dependencies=[adae]
     )
-    # Create task that generates TFL report from ADVS dataset
+    # Create task that generates TFL report from T_VSCAT table
     t_vscat = create_tfl_report(
         name="T_VSCAT", 
-        command="sas -stdio prod/tfl/t_ae_rel.sas", 
+        command="prod/tfl/t_ae_rel.sas", 
         environment="SAS Analytics Pro",
         hardware_tier= "Small",
-        dependencies=[ADAM("advs", advs)]
+        dependencies=[advs]
     )
-    return tfl_outputs(t_ae_rel=t_ae_rel, t_vscat=t_vscat)
-
-@workflow
-def adam_tfl(sdtm_data_path: str) -> tfl_outputs:
-    """
-    This script mocks a sample clinical trial using Domino Flows. 
-
-    The input to this flow is the path to your SDTM data. You can point this to either your SDTM-BLIND dataset or your SDTM-UNBLIND dataset. The output to this flow are a series of TFL reports.
-
-    To the run the workflow remotely, execute the following code in your terminal:
-    
-    pyflyte run --copy-all --remote workflow_adam_tfl.py adam_tfl --sdtm_data_path "/mnt/imported/data/snapshots/sdtm-blind/1"
-
-    :param sdtm_data_path: The root directory of your SDTM dataset
-    :return: A list of PDF files containing the TFL reports
-    """
-
-    adam_datasets = create_adam_datasets(sdtm_data_path=sdtm_data_path)
-    tfl_reports = create_tfl_reports(adsl=adam_datasets.adsl, adae=adam_datasets.adae, advs=adam_datasets.advs)
-
-    return tfl_reports
+    # Create task that generates TFL report from T_CONMED table
+    t_conmed = create_tfl_report(
+        name="T_CONMED", 
+        command="prod/tfl/t_conmed.sas", 
+        environment="SAS Analytics Pro",
+        hardware_tier= "Small",
+        dependencies=[adcm]
+    )
+     # Create task that generates TFL report from T_DEMOG table
+    t_demog = create_tfl_report(
+        name="T_DEMOG", 
+        command="prod/tfl/t_demog.sas", 
+        environment="SAS Analytics Pro",
+        hardware_tier= "Small",
+        dependencies=[adsl]
+    )
+     # Create task that generates TFL report from T_EFF table
+    t_eff = create_tfl_report(
+        name="T_EFF", 
+        command="prod/tfl/t_eff.sas", 
+        environment="SAS Analytics Pro",
+        hardware_tier= "Small",
+        dependencies=[adef]
+    )
+     # Create task that generates TFL report from T_SAF table
+    t_saf = create_tfl_report(
+        name="T_SAF", 
+        command="prod/tfl/t_saf.sas", 
+        environment="SAS Analytics Pro",
+        hardware_tier= "Small",
+        dependencies=[adsl, adae]
+    )
+      # Create task that generates TFL report from T_VITALS table
+    t_vitals = create_tfl_report(
+        name="T_VITALS", 
+        command="prod/tfl/t_vitals.sas", 
+        environment="SAS Analytics Pro",
+        hardware_tier= "Small",
+        dependencies=[advs]
+    )
+     # Create task that generates TFL report from L_MEDHIST list
+    l_medhist = create_tfl_report(
+        name="L_MEDHIST", 
+        command="prod/tfl/l_medhist.sas", 
+        environment="SAS Analytics Pro",
+        hardware_tier= "Small",
+        dependencies=[advs]
+        
+    )
+    # Create task that generates TFL report from L_MEDHIST list
+    combine_pdfs = create_tfl_report(
+        name="COMBINE_PDFs", 
+        command="utilities/combine_tfl.py", 
+        environment="GxP Validated R & Py",
+        hardware_tier= "Small",
+        dependencies=[t_ae_rel, t_conmed, t_demog, t_eff, t_pop, t_saf, t_vitals, t_vscat]
+    )    
+    return t_ae_rel, t_vscat, t_conmed, t_demog, t_eff, t_saf, t_vitals, l_medhist, combine_pdfs
